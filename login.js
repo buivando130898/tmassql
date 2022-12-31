bcc400ae0d6d2086411820d02bf072f0c3e86467
buivando130898 <51377919+buivando130898@users.noreply.github.com>
2022-12-31 10:47:53 +0700

    Update login.js

--- login.js
+++ login.js
@@ -3,14 +3,18 @@ server_get = "https://phanluong.t-matsuoka.com/api/get/login.php/";
 function login() {
     user = document.getElementById("user").value;
     pass = document.getElementById("pass").value;
-    const endpoint = `${server_get}/login_admin?user=${user}&pass=${pass}`;
+    clinic_id = document.getElementById("clinic_id").value;
+
+    const endpoint = `${server_get}/login_clinic?user=${user}&pass=${pass}&clinic_id=${clinic_id}`;
     // console.log(endpoint);
     fetch(endpoint)
         .then((response) => response.json())
         .then((data) => {
-            localStorage.setItem('acc_admin', data[0].acc);
-            localStorage.setItem('token_login_admin', data[0].token);
-            window.location = "./check.html";
+            // console.log(data[0].stt);
+            localStorage.setItem('clinic', clinic_id);
+			localStorage.setItem('acc_clinic', user);
+            localStorage.setItem('token_login_clinic', data[0].token);
+            window.location = "./clinic.html";
         }).catch((error) => {
             //// console.log("erro");
             document.getElementById("info_erro").innerHTML = "Tài khoản hoặc mật khẩu không chính xác";
