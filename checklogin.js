f6843e16eb5e12f728b9d36cc898429325321e32
buivando130898 <51377919+buivando130898@users.noreply.github.com>
2022-12-31 10:50:42 +0700

    Update checklogin.js

--- checklogin.js
+++ checklogin.js
@@ -1,17 +1,16 @@
 server_get = "https://phanluong.t-matsuoka.com/api/get/login.php/";
-if (localStorage['acc_clinic'] == undefined) {
+if (localStorage['acc_cskh'] == undefined) {
     window.location = "./login.html";
 } else {
-	acc_check =  localStorage['acc_clinic'];
-	token_check = localStorage["token_login_clinic"];
+	acc_check =  localStorage['acc_cskh'];
+	token_check = localStorage["token_login_cskh"];
 }
+ 
 
 const endpoint = `${server_get}/tokenlogin_check?acc=${acc_check}&token=${token_check}`;
-		console.log(endpoint);
 		fetch(endpoint)
 			.then((response) => response.json())
 			.then((data) => {
-				console.log(data);
 				if(data == false || data == "false") {
 					window.location = "./login.html";
 				} else {
